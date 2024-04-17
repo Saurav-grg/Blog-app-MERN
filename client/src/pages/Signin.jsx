22e918be2ca427bb8597b1f88c17efa05393d20b
Saurav-grg <menemy2323@gmail.com>
2024-04-17 16:58:21 +0545

    mmin

--- client/src/pages/Signin.jsx
+++ client/src/pages/Signin.jsx
@@ -3,10 +3,8 @@ import { FaGoogle } from 'react-icons/fa';
 
 export default function Signin() {
   const google = () => {
-    window.open(
-      'https://zenquest-api.vercel.app/api/auth/google/callback',
-      '_self'
-    );
+    // window.open('/api/auth/google/callback', '_self');
+    window.location.href('/api/auth/google/callback');
   };
   return (
     <div className="flex flex-col shadow-xl rounded-xl w-2/4 mx-auto min-h-[300px] my-8">
--- client/vite.config.js
+++ client/vite.config.js
@@ -8,9 +8,7 @@ export default defineConfig({
     proxy: {
       '/api': {
         target: 'https://zenquest-api.vercel.app',
-        // target: ' http://localhost:5000',
         changeOrigin: true,
-        // rewrite: (path) => path.replace(/^\/api/, ''),
       },
     },
   },
