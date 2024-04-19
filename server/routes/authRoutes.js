c1624105072e774959ac8abea48267476a441547
Saurav-grg <menemy2323@gmail.com>
2024-04-19 18:59:57 +0545

    login success check

--- server/routes/authRoutes.js
+++ server/routes/authRoutes.js
@@ -12,7 +12,7 @@ router.get('/login/failed', (req, res) => {
   });
 });
 router.get('/login/success', (req, res) => {
-  if (req.isAuthenticated()) {
+  if (req.user) {
     res.status(200).json({
       success: true,
       message: 'Login successful',
